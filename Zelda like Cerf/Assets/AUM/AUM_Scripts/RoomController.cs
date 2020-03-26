6e6d399dc68b12df9c10fe05a67035cf9db463ed
Aurélien <orelien.mary@gmail.com>
2020-03-26 14:56:36 +0100

    Merge Valentine

---
+++ Zelda like Cerf/Assets/AUM/AUM_Scripts/RoomController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    public List<SpriteRenderer> limitSRs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < limitSRs.Count; i++)
+        {
+            limitSRs[i].enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
