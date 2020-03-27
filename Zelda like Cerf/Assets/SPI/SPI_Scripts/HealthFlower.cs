496a6b667a11145101dad7f1aa6cfe985466a4f9
Cardo <S.picardat@rubika-edu.com>
2020-03-27 16:54:42 +0100

    Spikes,HealthFlower & CrackedWall Added

---
+++ Zelda like Cerf/Assets/SPI/SPI_Scripts/HealthFlower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthFlower : Elements_Controller
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player_Main_Controller player = collision.GetComponentInParent<Player_Main_Controller>();
+        if (player != null)
+        {
+            player.life++;
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Zelda like Cerf/Assets/SPI/SPI_Scripts/Spike.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    public int baseDamage;
+    [HideInInspector] public float damagetaken;
+
+    int detectedObjectMass;
+    float detectedObjectProjectionLevel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Elements_Controller element = collision.GetComponentInParent<Elements_Controller>();
+        
+        Ennemy_Controller ennemyspike = collision.GetComponentInParent<Ennemy_Controller>();
+        if (element != null)
+        {
+            detectedObjectMass = element.mass;
+            detectedObjectProjectionLevel = element.levelProjected;
+        }
+        if (collision.attachedRigidbody.CompareTag("Player"))
+        {
+            collision.attachedRigidbody.GetComponent<Player_Main_Controller>().StartCoroutine(collision.attachedRigidbody.GetComponent<Player_Main_Controller>().TakeDamage(baseDamage));
+        }
+        if (ennemyspike != null)
+        {
+            damagetaken = (detectedObjectProjectionLevel * 2) / detectedObjectMass;
+            collision.GetComponentInParent<Ennemy_Controller>().StartTakeDamage(damagetaken);
+        }
+    }
+}
