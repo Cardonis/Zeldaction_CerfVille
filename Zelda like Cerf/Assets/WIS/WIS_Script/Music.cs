cf4527444041c01bd783962c0e3c342465865dc9
William <w.schmitt@rubika-edu.com>
2020-04-26 15:11:39 +0200

    Music Manager

---
+++ Zelda like Cerf/Assets/WIS/WIS_Script/Music.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Music
+{
+    public string name;
+
+    public AudioClip Clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    public bool loop;
+    public bool IsPlaying;
+
+    [HideInInspector]
+    public AudioSource source;
+
+
+
+}
