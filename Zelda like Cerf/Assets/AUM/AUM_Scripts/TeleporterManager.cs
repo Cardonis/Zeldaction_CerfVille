d897141291c6a6026ecf42a4b1585e9f8b5f7606
Aurélien <orelien.mary@gmail.com>
2020-04-28 23:38:23 +0200

    Teleporter + LevelUp

---
+++ Zelda like Cerf/Assets/AUM/AUM_Scripts/TeleporterManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterManager : MonoBehaviour
+{
+    public int teleporterNumber = 0;
+    public int teleportPart = 0;
+    public bool canTeleport = false;
+
+    public void SetTeleporterNumber(int telNumb)
+    {
+        teleporterNumber = telNumb;
+        canTeleport = true;
+    }
+
+    public void SetPartNumber(int telPart)
+    {
+        teleportPart = telPart;
+    }
+
+}
