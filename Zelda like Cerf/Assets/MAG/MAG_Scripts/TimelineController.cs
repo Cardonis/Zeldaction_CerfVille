cb08eec12c0888afea1645ea2c947df2fb83a291
Manon Ghignoni <m.ghignoni@rubika-edu.com>
2020-05-08 12:00:58 +0200

    Correctif cinématique intro

---
+++ Zelda like Cerf/Assets/MAG/MAG_Scripts/TimelineController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public Player_Main_Controller player;
+
+    private void Start()
+    {
+        Debug.Log("oui");
+        player.StartCoroutine(player.StunnedFor(17));
+    }
+    public void Play()
+    {
+        playableDirector.Play();
+    }
+}
