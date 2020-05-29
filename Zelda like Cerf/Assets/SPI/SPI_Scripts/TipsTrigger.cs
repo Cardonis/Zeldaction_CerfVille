e9c72bb7ccd87f649f0007313395c7f1033abac6
Cardo <S.picardat@rubika-edu.com>
2020-05-29 09:52:17 +0200

    Tips gif WIP

---
+++ Zelda like Cerf/Assets/SPI/SPI_Scripts/TipsTrigger.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipsTrigger : MonoBehaviour
+{
+
+    Player_Main_Controller player;
+
+    bool playerIsNear=false;
+    public bool asTalked;
+
+
+    public TipsPlayer tipsPlayer;
+
+    public OutlineController outlineController;
+
+    public GameObject theTips;
+
+    private void Update()
+    {
+
+        if (Input.GetButtonDown("X") && tipsPlayer.isInTips == false && playerIsNear == true && asTalked == false)
+        {
+            asTalked = true;
+            player.pressX.SetActive(false);
+            StartCoroutine(tipsPlayer.OpenTips(theTips));
+
+            outlineController.outLining = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Player_Main_Controller p = collider.transform.parent.parent.GetComponent<Player_Main_Controller>();
+        if (p != null)
+        {
+            player = p;
+            playerIsNear = true;
+            player.pressX.SetActive(true);
+
+            outlineController.outLining = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        Player_Main_Controller player = collider.transform.parent.parent.GetComponent<Player_Main_Controller>();
+        if (player != null)
+        {
+            player.pressX.SetActive(false);
+            playerIsNear = false;
+            asTalked = false;
+
+            outlineController.outLining = false;
+        }
+
+    }
+}
