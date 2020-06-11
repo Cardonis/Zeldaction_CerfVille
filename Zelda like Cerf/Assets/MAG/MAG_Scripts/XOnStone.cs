bfc0b8223b354c56de82d21189ef975ac3bd5428
Manon Ghignoni <m.ghignoni@rubika-edu.com>
2020-06-11 21:33:21 +0200

    Correction de bugs

--- Zelda like Cerf/Assets/MAG/MAG_Scripts/XOnStone.cs
+++ Zelda like Cerf/Assets/MAG/MAG_Scripts/XOnStone.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class XOnStone : MonoBehaviour
 {
     Player_Main_Controller truePlayer;
+    Caisse_Controller pierre;
+
+    private void Start()
+    {
+        pierre = gameObject.GetComponent<Caisse_Controller>();
+    }
     public void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.attachedRigidbody != null)
         {
             Player_Main_Controller player = collider.attachedRigidbody.GetComponent<Player_Main_Controller>();
-            if (player != null)
+            if (player != null && pierre.isTractable == true)
             {
                 truePlayer = player;
                 truePlayer.pressX.SetActive(true);
             }
         }
-        
-
 
     }
     public void OnTriggerExit2D(Collider2D collider)
@@ -30,7 +34,5 @@ public class XOnStone : MonoBehaviour
                 truePlayer.pressX.SetActive(false);
             }
         }
-
-        
     }
 }
