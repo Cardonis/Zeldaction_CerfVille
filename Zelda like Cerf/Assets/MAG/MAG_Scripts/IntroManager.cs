5b0c77eb2f666d8c6963ca8008a384871ceaf19e
Manon Ghignoni <m.ghignoni@rubika-edu.com>
2020-06-12 16:48:44 +0200

    Coorectifs cartes + intro jeu + new typo

---
+++ Zelda like Cerf/Assets/MAG/MAG_Scripts/IntroManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject intro;
+    public AnimationClip logoRubikaClip;
+
+    bool stratingToFade;
+
+    float timerLogo;
+    float timer;
+
+    public Animator fondAnimator;
+
+    private void Start()
+    {
+        timerLogo = logoRubikaClip.length;
+        stratingToFade = false;
+    }
+    private void Update()
+    {
+       timer += Time.deltaTime;
+        if(timer> timerLogo)
+        {
+            menu.SetActive(true);
+            stratingToFade = true;
+            fondAnimator.SetBool("StartFading", stratingToFade);
+        }
+    }
+}
