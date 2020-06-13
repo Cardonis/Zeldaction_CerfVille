1da33726c0b7bee48b2e08ca2efa47a0238bc21b
Manon Ghignoni <m.ghignoni@rubika-edu.com>
2020-06-13 20:12:09 +0200

    Bug Fix

---
+++ Zelda like Cerf/Assets/MAG/MAG_Scripts/ArbreAyen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArbreAyen : MonoBehaviour
+{
+
+    public GameObject arbusteAmiMortPetit;
+    public GameObject arbusteAmiMortGrand;
+    public Player_Main_Controller player;
+
+    private void Start()
+    {
+        if (player.part >= 2)
+        {
+            arbusteAmiMortPetit.SetActive(false);
+            arbusteAmiMortGrand.SetActive(true);
+
+        }
+    }
+}
